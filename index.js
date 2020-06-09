7479c302faa7574d6a6e0b32eeeba1580f16b170
Yididya Samuel <yidisam18@gmail.com>
2020-06-08 22:11:42 -0400

    node_farm

---
+++ index.js
@@ -0,0 +1,88 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// Blocking Synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `${textIn}\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File Written");
+
+// Non-Blocking, Asynchronous
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+// if (err) throw err;
+// console.log(data);
+// });
+
+// create server
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const data = fs.readFileSync("./dev-data/data.json", "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // overview page
+  if (req.url === "/" || req.url === "overview") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const cardsHtml = dataObj
+      .map((data) => replaceTemplate(tempCard, data))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    // console.log(cardsHtml);
+
+    res.end(output);
+    // res.end ('This is the Overview');
+    // product page
+  } else if (req.url === "/product") {
+    res.end("This is the products Page!");
+    //API
+  } else if (req.url === "/api") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(data);
+    // Not Found
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "application/json",
+    });
+    res.end("Page Not Found!");
+  }
+});
+
+server.listen(9000, () => {
+  console.log("Server started on port 9000");
+});
