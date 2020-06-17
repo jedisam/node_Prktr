6b6546d35111934817e66c91bfd165b93cb21f2d
Yididya Samuel <yidisam18@gmail.com>
2020-06-17 18:43:02 -0400

    final model structure

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,21 +1,58 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema ({
+const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour name is required'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: String,
+    required: [true, 'A tour mush have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour mush have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // eslint-disable-next-line no-multi-assign
-const Tour = mongoose.model ('Tour', tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
