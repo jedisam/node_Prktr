acff93e930ff03db581811dc5c0cceea700e323d
Yididya Samuel <yidisam18@gmail.com>
2020-06-17 22:04:48 -0400

    clear db & insert from file to db

---
+++ natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,55 @@
+const dotenv = require('dotenv');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+// require("dotenv/config");
+// console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connected successfully!');
+  });
+
+// READ_FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import Data Inot DB
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data Successfully Created');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Delete all data from collection
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
--- natours/server.js
+++ natours/server.js
@@ -1,27 +1,26 @@
-const dotenv = require ('dotenv');
-const mongoose = require ('mongoose');
-const app = require ('./app.js');
-const Tour = require ('./models/tourModel');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app.js');
 // require("dotenv/config");
 // console.log(process.env);
-dotenv.config ({path: './config.env'});
+dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE;
 
 mongoose
-  .connect (DB, {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then (() => {
-    console.log ('DB connected successfully!');
+  .then(() => {
+    console.log('DB connected successfully!');
   });
 
 const PORT = 9000;
 
-app.listen (PORT, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log (`Server started on Port ${PORT}...`);
+  console.log(`Server started on Port ${PORT}...`);
 });
